10b5b24a3f5563bf1741003c2475b9e1f4e3569f
Mohammad haikhal maudary <93951063+Kalme99@users.noreply.github.com>
2022-09-19 00:18:35 +0800

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "E:\\belajarFlutter\\portofolio_1" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\flutter"
+  "PROJECT_DIR=E:\\belajarFlutter\\portofolio_1"
+  "FLUTTER_ROOT=D:\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=E:\\belajarFlutter\\portofolio_1\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=E:\\belajarFlutter\\portofolio_1"
+  "FLUTTER_TARGET=E:\\belajarFlutter\\portofolio_1\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=E:\\belajarFlutter\\portofolio_1\\.dart_tool\\package_config.json"
+)
